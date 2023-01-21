0441c8a7d6c7bb1070bfd2a220566dc3bac3090b
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2023-01-21 16:39:57 +0100

    retype inputs

--- src/LineChartSetters.php
+++ src/LineChartSetters.php
@@ -9,7 +9,7 @@ trait LineChartSetters {
 
     private int $heightSvg = 30;
 
-    private int $strokeWidth = 2;
+    private float $strokeWidth = 2;
 
     private bool $reverseOrder = false;
 
@@ -18,7 +18,7 @@ trait LineChartSetters {
     /** @var string[] */
     private array $colors = ['#ED7D31', '#E41A5C'];
 
-    public function withDimensions(?int $widthSvg = null, ?int $heightSvg = null): self {
+    public function withDimensions(mixed $widthSvg = null, mixed $heightSvg = null): self {
         $clone = clone $this;
         $clone->widthSvg = $this->positiveIntOrNull($widthSvg) ?? $clone->widthSvg;
         $clone->heightSvg = $this->positiveIntOrNull($heightSvg) ?? $clone->heightSvg;
@@ -26,9 +26,9 @@ trait LineChartSetters {
         return $clone;
     }
 
-    public function withStrokeWidth(?int $strokeWidth = null): self {
+    public function withStrokeWidth(mixed $strokeWidth = null): self {
         $clone = clone $this;
-        $clone->strokeWidth = $this->positiveIntOrNull($strokeWidth) ?? $clone->strokeWidth;
+        $clone->strokeWidth = $this->positiveFloatOrNull($strokeWidth) ?? $clone->strokeWidth;
 
         return $clone;
     }
@@ -40,7 +40,7 @@ trait LineChartSetters {
         return $clone;
     }
 
-    public function withMaxItemAmount(?int $maxItemAmount = null): self {
+    public function withMaxItemAmount(mixed $maxItemAmount = null): self {
         $clone = clone $this;
         $clone->maxItemAmount = $this->positiveIntOrNull($maxItemAmount) ?? $clone->maxItemAmount;
 
@@ -60,6 +60,18 @@ trait LineChartSetters {
             is_bool($value) => (int) $value,
             is_numeric($value) => (int) $value,
             (is_string($value) && ctype_digit($value)) => (int) $value,
+            default => 0,
+        };
+
+        return abs($value) > 0 ? abs($value) : null;
+    }
+
+    private function positiveFloatOrNull(mixed $value): ?int {
+        $value = match (true) {
+            is_int($value) => (float) $value,
+            is_bool($value) => (float) $value,
+            is_numeric($value) => (float) $value,
+            (is_string($value) && ctype_digit($value)) => (float) $value,
             default => 0,
         };
 
